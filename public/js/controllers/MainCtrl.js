1b6ddce345d1481defbcaea746cf19486f62d833
elliot richardson <ear78@aol.com>
2016-11-16 13:39:43 -0600

    working on mobile menu dropdown directives

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,10 +1,23 @@
 angular.module('myApp')
     .controller('MainCtrl', function($scope){
 
-        // function for about us mobile dropdown
+        // functions for about us mobile dropdown
         $scope.showAboutUs = false;
         $scope.toggleAboutUs = function(){
             $scope.showAboutUs = !$scope.showAboutUs;
+
+        }
+
+        // functions for Discover mobile dropdown
+        $scope.showDiscover = false;
+        $scope.toggleDiscover = function(){
+            $scope.showDiscover = !$scope.showDiscover;
+        }
+
+        //function for Customer service dropdown
+        $scope.showCustomerService = false;
+        $scope.toggleCustomerService = function(){
+            $scope.showCustomerService = !$scope.showCustomerService;
         }
 
 
