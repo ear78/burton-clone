3f64877cad7701d682f7f4995f86f363aab2e00e
elliot richardson <ear78@aol.com>
2016-11-23 11:40:30 -0600

    added functionality to sidemenu close window and button using an ng-show

--- public/js/directives/footerDirective.js
+++ public/js/directives/footerDirective.js
@@ -2,6 +2,32 @@ angular.module('myApp')
   .directive('footerDirective', function(){
     return {
       restrict: 'E',
-      templateUrl: '../../views/directives/footerDirective.html'
+      templateUrl: '../../views/directives/footerDirective.html',
+      link: function(scope, elem, attr){
+        // functions for about us mobile dropdown
+        scope.showAboutUs = false;
+        scope.toggleAboutUs = function(){
+            scope.showAboutUs = !scope.showAboutUs;
+
+        }
+
+        // functions for Discover mobile dropdown
+        scope.showDiscover = false;
+        scope.toggleDiscover = function(){
+            scope.showDiscover = !scope.showDiscover;
+        }
+
+        //function for Customer service dropdown
+        scope.showCustomerService = false;
+        scope.toggleCustomerService = function(){
+            scope.showCustomerService = !scope.showCustomerService;
+        }
+
+        //SIDEMENU CLOSE FUNCTIONS
+        scope.hideSideMenuModal = false;
+        scope.closeSideMenu = function(){
+          scope.hideSideMenuModal = !scope.hideSideMenuModal;
+        }
+      }
     };
   });
