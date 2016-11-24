43ac290aef7bac4f05d21a885a33fef12770b3da
elliot richardson <ear78@aol.com>
2016-11-24 00:58:39 -0600

    finished menu with animations, and finished footer menu animations

--- public/js/directives/headerDirective.js
+++ public/js/directives/headerDirective.js
@@ -6,16 +6,21 @@ angular.module('myApp')
       link: function(scope, elem, attr){
         //HIDE SIDEMENU
         scope.hideSideMenu = true;
+        //TOGGLE CLASS FOR CLOSE BUTTON ANIMATIONS
+        scope.toggle = false;
 
         //OPEN SIDEMENU
         scope.openSideMenu = function(){
           scope.hideSideMenu = !scope.hideSideMenu;
+          scope.toggle = !scope.toggle;
         }
 
         //SIDEMENU CLOSE FUNCTIONS
         scope.closeSideMenu = function(){
           scope.hideSideMenu = !scope.hideSideMenu;
+          scope.toggle = !scope.toggle;
         }
+
       }
     };
   });
